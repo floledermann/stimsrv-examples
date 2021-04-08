aeca0faf28a323357bec457b4d6e9f1d1c20a307
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-04-08 15:01:57 +0200

    improved launch script

--- examples/loop/launch.bat
+++ examples/loop/launch.bat
@@ -1,2 +1,31 @@
-call npx stimsrv %1
+@echo off
+echo. 
+
+setlocal
+
+set filename=%1
+
+set EXPERIMENT_PATTERN=*experiment*.js
+
+if NOT "%filename%"=="" goto ok
+REM default filename: first js file with "experiment" in name
+FOR %%F IN (%EXPERIMENT_PATTERN%) DO (
+  echo Experiment matching pattern found: %%F
+  set filename=%%F
+  goto ok
+)
+echo No experiment (matching pattern %EXPERIMENT_PATTERN%) found in current directory!
+echo.
+pause
+exit /b 1
+:ok
+
+
+echo Launching %filename% ...
+echo.
+
+set command=npx stimsrv --open %filename%
+echo %command%
+call %command%
+
 pause
