8a09d37cd4383a3ba3874f007834fd96aa2c7ac2
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-06-11 13:27:40 +0200

    add slippy map task to simple-browser example

--- examples/simple-browser/experiment-simple-browser.js
+++ examples/simple-browser/experiment-simple-browser.js
@@ -2,6 +2,7 @@
 const pause = require("stimsrv/task/pause");
 const sloan = require("stimsrv/task/sloan");
 const text = require("./text.js");
+const mapTask = require("stimsrv-task-slippymap");
 
 const sequence = require("stimsrv/controller/sequence");
 
@@ -19,7 +20,7 @@ module.exports = {
   
   devices: [
     {
-      id: "dev",
+      id: "anyone",
       resolution: "hd",
       imageSize: "600x600",
       pixeldensity: 91,
@@ -34,6 +35,16 @@ module.exports = {
       viewingdistance: 600,
     },
     {
+      id: "kindle",
+      client: "browser-simple",
+      imageType: "jpeg",
+      imageQuality: 40,
+      resolution: "hd",
+      imageSize: "1260x1480",
+      pixeldensity: 91,
+      viewingdistance: 350,
+    },
+    {
       id: "old2",
       client: "browser-canvas",
       resolution: "hd",
@@ -45,16 +56,22 @@ module.exports = {
   
   roles: [
     {
+      role: "display",
+      description: "Stimulus display on modern browsers",
+      interfaces: ["display"],
+      devices: ["anyone"]
+    },
+    {
       role: "display-oldbrowser",
       description: "Stimulus display on old browser",
       interfaces: ["display"],
-      devices: ["anyone","dev","old"]
+      devices: ["anyone","old","kindle","old2"]
     },
     {
       role: "response",
       description: "Response",
       interfaces: ["response"],
-      devices: ["anyone","dev"]
+      devices: ["anyone"]
     },
   ],
     
@@ -68,11 +85,36 @@ module.exports = {
         fontSize: sequence(["10mm","15mm"])
       },
       fonts: [{
-          family: "Orelega One",
-          resource: "OrelegaOne-Regular.ttf"
+        family: "Orelega One",
+        resource: "OrelegaOne-Regular.ttf"
       }]
     }), 
-    
+
+    // even complex UIs like a slippy map can be shown on simple browsers
+    // make sure to generate a response whenever the map should update, because this triggers re-rendering for old browsers
+    mapTask({
+      tiles: {
+        tileURL: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 19
+      },
+      synchronize: true,
+      // define task functions for map task - by default, slippymap task doesn't define these
+      initialize: (parent, stimsrv, map, context) => {
+        // only trigger when *user* moves the map, not when moved through sync
+        map.addUserEventListener("moveend", () => stimsrv.response({center:map.getCenter(),zoom:map.getZoom()}));
+      },
+      render: (condition, map, context) => {
+        map.setView(condition.center, condition.zoom);
+      },
+      controller: context => ({
+        // send out the response as condition, to trigger sync of old browsers
+        nextCondition(lastCondition, response) {
+          return response || {center:[0,0],zoom:2};
+        }
+      })
+    }),
+        
     pause({
       message: "Restart...",
     }),
