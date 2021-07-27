1844209967d353f0459ceabc2aa08fc68138e738
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-07-27 15:14:03 +0200

    rename examples to put basic ones in front

--- examples/a_minimal/experiment-minimal.js
+++ examples/a_minimal/experiment-minimal.js
@@ -10,6 +10,11 @@ module.exports = {
     pause({
       message: "Hello from stimsrv",
       store: true  // by default, the pause task is not stored - store it so that we have some data
+    }),
+    
+    pause({
+      message: "Goodbye from stimsrv",
+      store: true
     })
     
   ]
