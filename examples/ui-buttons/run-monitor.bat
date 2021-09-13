584eefaf1f73273b86857fb1a7889fa255e5aa64
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-09-13 16:05:50 +0200

    add "smart" launch script

--- examples/ui-buttons/run-monitor.bat
+++ examples/ui-buttons/run-monitor.bat
@@ -1,3 +1,19 @@
-npx nodemon -e js --watch . --exec "npx" stimsrv experiment-buttons.js
+@echo off
+setlocal
 
+set filename=%1
+set EXPERIMENT_PATTERN=experiment*.js
+
+if NOT "%filename%"=="" goto ok
+FOR %%F IN (%EXPERIMENT_PATTERN%) DO (
+  set filename=%%F
+  goto ok
+)
+echo No experiment (matching pattern %EXPERIMENT_PATTERN%) found in current directory!
+pause
+exit /b 1
+:ok
+
+echo Launching %filename% ...
+call npx nodemon -e js --watch . --exec "npx" stimsrv %filename%
 pause
